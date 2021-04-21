5a1ca7e3ae0dbb3f9d07b1c86749368ca498a055
Rohit275 <rohit.a.2k@gmail.com>
2021-04-21 13:43:13 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import chatbot from './chatbot.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper">
+          <div className="logo">
+            <img src="https://i.postimg.cc/mg4rWBmv/logo.png" alt="logo" />
+          </div>
+          <ul className="nav-area">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div className="welcome-text">
+          <h1><span>Nature</span> is the art of god</h1>
+          <a href="#">Contact US</a>
+        </div>
       </header>
+      <chatbot />
     </div>
   );
 }
